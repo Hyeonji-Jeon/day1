3052f28233e35fcd54cfb3f645385100fb65e521
Hyeonji Jeon <guswl1397@naver.com>
2025-01-06 13:33:04 +0900

    1st

--- src/main/java/org/example/RPSGame.java
+++ src/main/java/org/example/RPSGame.java
@@ -1,11 +1,19 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class RPSGame {
 
     public static void main(String[] args) {
 
-        int user = 2; //사용자 값
-        int com = 0; // 컴 값
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("가위0, 바위1, 보2 를 입력하세요");
+        String str = scanner.nextLine(); //키보드에서 입력한 문자열
+        System.out.println(str);
+
+        int user = Integer.parseInt(str);
+        int com = (int)(Math.random()*3); // 컴 값
 
         //삼항연산자
         //user = user < com ? user + 3 : user;
@@ -16,14 +24,17 @@ public class RPSGame {
 
         int result = user - com; //0,1,2
 
+        System.out.println("USER: " + user);
+        System.out.println("COM: " + com);
+
         switch (result){
             case 0:
                 System.out.println("비김");
                 break;
-            case 1;
+            case 1:
                 System.out.println("이김");
                 break;
-            case 2;
+            case 2 :
                 System.out.println("졌다");
                 break;
         }//end switch
